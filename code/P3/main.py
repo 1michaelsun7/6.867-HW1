7b9b0c3e75ae7a249aa3960348fae85774f48fe9
Michael Sun <micsun@mit.edu>
2016-09-27 10:05:22 -0400

    3.1

--- code/P3/main.py
+++ code/P3/main.py
@@ -1,6 +1,33 @@
 import numpy as np
-import math import random 
+import math 
+import random 
 import matplotlib.pyplot as plt
+import loadFittingDataP2 as loadfd
+import ridge_regression as rr
 
-#Set the different values of alpha to be tested
-alpha_ridge = [1e-15, 1e-10, 1e-8, 1e-4, 1e-3,1e-2, 1, 5, 10, 20]
+def plot(func):
+    plt.plot(X,Y,'o')
+    plt.plot(X,np.cos(np.pi*X)+np.cos(np.pi*2*X))
+    plt.plot(X, func)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.show()
+
+
+if __name__ == "__main__":
+	(X, Y) = loadfd.getData(False)
+	#Set the different values of alpha to be tested
+	alpha_ridge = [1e-15, 1e-10, 1e-8, 1e-4, 1e-3,1e-2, 1, 5, 10, 20]
+
+	best_loss = float('inf')
+	best_params = (None, None, None)
+	for alpha in alpha_ridge:
+		phi, w = rr.ridge_regression(X, Y, 1, alpha)
+
+		reg_error = 0.5 * np.sum(np.square(np.dot(phi, w) - Y)) + alpha*np.sum(np.square(w))
+		if reg_error < best_loss:
+			best_loss = reg_error
+			best_params = (phi, w, alpha)
+
+	print "Best Alpha: ", best_params[2]
+	plot(np.dot(best_params[0], best_params[1]))
---
+++ code/P3/ridge_regression.py
@@ -0,0 +1,23 @@
+import numpy as np
+import math 
+import random 
+
+def ridge_regression(X, Y, M, lam):
+	phi = basis(X, M)
+
+	phi_square = np.dot(np.transpose(phi), phi)
+	id_matrix = np.identity(phi_square.shape[0])
+
+	phi_target = np.dot(np.transpose(phi), Y)
+
+	weight_vect = np.dot(np.linalg.inv(lam*id_matrix + phi_square), phi_target)
+
+	return phi, weight_vect
+
+def basis(X, M):
+	M += 1
+	N = len(X)
+	phi = np.zeros((N,M))
+	for col in xrange(M):
+		phi[:,col] = np.power(X, col)
+	return phi
