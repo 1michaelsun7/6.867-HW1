092675b0d13718e29e0001faf8570672c5237739
Laura Pang <Laura Pang>
2016-09-28 01:21:28 -0400

    2.2 / 2.4

--- code/P2/main.py
+++ code/P2/main.py
@@ -1,56 +1,53 @@
 import loadFittingDataP2 as loadfd
 import numpy as np
 import matplotlib.pyplot as plt
-
+import regression as rg
 
 """
 Created on Mon Sep 26 23:04:30 2016
 
 @author: Laura Pang
 """
-def f(X, Y, M):  
-    M+=1
-    print 'X: ', X
-    print 'Y: ', Y
-    N = len(X)
-    phi = np.zeros((N,M))
-    for col in xrange(M):
-        phi[:,col] = np.power(X, col)
-#    print phi
-    return phi
-    
-def SSE(X, Y, w, phi):
-    N = len(X)
-    tot = 0
-    for j in range(N):
-        tot+= (Y[j] - np.dot(phi[j], w))**2
-    return 0.5*tot
 
-def SSEDeriv(X, Y, w, phi):
-    N = len(X)
-    tot = 0
-    for j in range(N):
-        tot+= (Y[j] - np.dot(phi[j], w))*phi[j] ##?????
-    return tot
-    
-def plot(func):
+def plot(X1, func):
 ##    xran = numpy.linspace(0,1,num=400)
     plt.plot(X,Y,'o')
-    plt.plot(X,np.cos(np.pi*X)+np.cos(np.pi*2*X))
-    plt.plot(X, func)
+    plt.plot(X1,np.cos(np.pi*X1)+np.cos(np.pi*2*X1))
+    plt.plot(X1, func)
     plt.xlabel('x')
     plt.ylabel('y')
     plt.show()
-    
-    
+  
 if __name__ == "__main__":
     #2.1
     (X, Y) = loadfd.getData(False)
-    phi = f(X, Y, 3)
+    phi = rg.f(X, Y, 1)
     w = np.dot(np.linalg.inv(np.dot(np.transpose(phi), phi)),np.dot(np.transpose(phi), Y))
     print 'w: ', w
-    plot(np.dot(phi,w))
+#    plot(np.dot(phi,w))
     
     #2.2
-    sse = SSE(X, Y, w, phi)
-    print sse
+    sse = rg.SSE(X, Y, w, phi)
+    ssed = rg.SSEDeriv(X, Y, w, phi)
+    
+    def fn(w1):
+        return rg.SSE(X,Y,w1,phi)
+        
+    print rg.approx_gradient(fn, w, 1e-6)
+    
+    #2.4
+    phi = rg.fCos(X, Y)
+    w = np.dot(np.linalg.inv(np.dot(np.transpose(phi), phi)),np.dot(np.transpose(phi), Y))
+    print 'w: ', w
+#    plot(np.dot(phi,w))
+    
+    xran = np.linspace(0,1,num=400)
+    phi2 = rg.fCos(xran, Y)
+#    print np.dot(phi2, w)
+#    plot(xran, np.dot(phi2, w)) ###SMOOTH CUURVES
+    
+    array_x = np.array(X)
+    array_y = np.array(Y)
+#    theta = batch_gradient_descent(array_x, array_y, x_init=np.random.random((10,)), lr=1e-4, max_iters=10000)
+#    print theta
+
