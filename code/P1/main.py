da46391c570107010941c9574ba85bee30d3b1f5
Michael Sun <micsun@mit.edu>
2016-09-28 13:38:29 -0400

    fixed sgd finally, adding p4

--- code/P1/main.py
+++ code/P1/main.py
@@ -16,27 +16,27 @@ def gradient_gaussian(f, mean, cov):
 
 if __name__ == "__main__":
     #1.1
-	(gaussMean,gaussCov,quadBowlA,quadBowlb) = loadpm.getData()
-	def func(x):
-		return 0.5*np.dot(np.transpose(x), np.dot(quadBowlA, x)) - np.dot(np.transpose(x), quadBowlb)
-	grad_qb = gradient_quadratic_bowl(quadBowlA, quadBowlb)
-	best_val = gd.functional_gradient_descent(func, np.random.random((2,)), f_prime=grad_qb)
-	print best_val
+	# (gaussMean,gaussCov,quadBowlA,quadBowlb) = loadpm.getData()
+	# def func(x):
+	# 	return 0.5*np.dot(np.transpose(x), np.dot(quadBowlA, x)) - np.dot(np.transpose(x), quadBowlb)
+	# grad_qb = gradient_quadratic_bowl(quadBowlA, quadBowlb)
+	# best_val = gd.functional_gradient_descent(func, np.random.random((2,)), f_prime=grad_qb)
+	# print best_val
 
-	def gaussfunc(x):
-		return -1.0/(math.sqrt(math.pow(2*math.pi, x.shape[0])*np.linalg.det(gaussCov)))*math.exp(-0.5*np.dot(np.dot(np.transpose(x-gaussMean),np.linalg.inv(gaussCov)),(x-gaussMean)))
-	grad_gauss = gradient_gaussian(gaussfunc, gaussMean, gaussCov)
-	best_val = gd.functional_gradient_descent(gaussfunc, np.ones((2,)))
-	print best_val
+	# def gaussfunc(x):
+	# 	return -1.0/(math.sqrt(math.pow(2*math.pi, x.shape[0])*np.linalg.det(gaussCov)))*math.exp(-0.5*np.dot(np.dot(np.transpose(x-gaussMean),np.linalg.inv(gaussCov)),(x-gaussMean)))
+	# grad_gauss = gradient_gaussian(gaussfunc, gaussMean, gaussCov)
+	# best_val = gd.functional_gradient_descent(gaussfunc, np.ones((2,)))
+	# print best_val
 
 	#1.2
 	# By not initializing f_prime, we will use the central difference approximation in grad_descent.py
-	(gaussMean,gaussCov,quadBowlA,quadBowlb) = loadpm.getData()
-	def func(x):
-		return 0.5*np.dot(np.transpose(x), np.dot(quadBowlA, x)) - np.dot(np.transpose(x), quadBowlb)
-	grad_qb = gradient_quadratic_bowl(quadBowlA, quadBowlb)
-	best_val = gd.functional_gradient_descent(func, np.random.random((2,)), f_prime=grad_qb)
-	print best_val
+	# (gaussMean,gaussCov,quadBowlA,quadBowlb) = loadpm.getData()
+	# def func(x):
+	# 	return 0.5*np.dot(np.transpose(x), np.dot(quadBowlA, x)) - np.dot(np.transpose(x), quadBowlb)
+	# grad_qb = gradient_quadratic_bowl(quadBowlA, quadBowlb)
+	# best_val = gd.functional_gradient_descent(func, np.random.random((2,)), f_prime=grad_qb)
+	# print best_val
 
 	#1.3
 	X, Y = loadfd.getData()
@@ -51,5 +51,5 @@ if __name__ == "__main__":
 	X, Y = loadfd.getData()
 	array_x = np.array(X)
 	array_y = np.array(Y)
-	theta = gd.stochastic_gradient_descent(array_x, array_y, x_init=np.random.random((10,)), lr=1e-2, lr2=10, max_iters=10000)
+	theta = gd.stochastic_gradient_descent(array_x, array_y, x_init=np.random.random((10,)), lr=100000, max_iters=10000)
 	print theta
--- code/P4/lasso.py
+++ code/P4/lasso.py
@@ -1,13 +1,23 @@
 import numpy as np
 import math 
 import random 
+from sklearn import linear_model as sklin
 
-def lasso(x, y, lambda):
+def lasso(x, y, lam):
 	phi = basis(x, 13)
 
-def basis(x, M):
-	phi = np.zeros((M,))
+	clf = sklin.Lasso(alpha = lam)
+	clf.fit(phi, y)
 
-	phi[0] = x
+	return clf.coef_
+
+def basis(x, M):
+	x = np.squeeze(x)
+	N = x.shape[0]
+	phi = np.zeros((N,M))
+
+	phi[:,0] = x
 	for i in xrange(1, M):
-		phi[i] = math.sin(0.4*math.pi*i*x)
+		phi[:,i] = np.sin(0.4*np.pi*i*x)
+
+	return phi
